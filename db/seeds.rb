0c01e9e27f9ca40d0c642ea21588657774078583
thierry vinagre <vinagre.thierry@yahoo.fr>
2020-03-11 12:04:34 +0100

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 puts 'Creating 5 products...'
 5.times do |i|
   artist = Artist.create!(
-    name: Faker::Artist.name,
+    name: Faker::Book.author,
     tagline: Faker::TvShows::SiliconValley.quote
   )
   puts "#{i + 1}. #{artist.name}"
